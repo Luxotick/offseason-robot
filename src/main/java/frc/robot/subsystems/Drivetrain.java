d17ea170cc664f37e6dc9c0f10f078fb00919498
Luxotick <mertistakenn@gmail.com>
2023-04-14 23:17:56 +0300

    made tankdrive controls

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CANIDConstants;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;;
+
+
+public class Drivetrain extends SubsystemBase {
+
+  CANSparkMax leftLeadMotor = new CANSparkMax(CANIDConstants.leftLeadMotorID, MotorType.kBrushed);
+  WPI_VictorSPX leftFollowMotor = new WPI_VictorSPX(CANIDConstants.leftFollowMotorID);
+  CANSparkMax rightLeadMotor = new CANSparkMax(CANIDConstants.rightLeadMotorID, MotorType.kBrushed);
+  WPI_VictorSPX rightFollowMotor = new WPI_VictorSPX(CANIDConstants.rightFollowMotorID);
+
+  MotorControllerGroup leftMotors = new MotorControllerGroup(leftLeadMotor, leftFollowMotor);
+  MotorControllerGroup rightMotors = new MotorControllerGroup(rightLeadMotor, rightFollowMotor);
+
+  DifferentialDrive differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {}
+
+  public void arcadeDrive(double moveSpeed, double rotateSpeed) {
+    differentialDrive.tankDrive(moveSpeed, rotateSpeed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
